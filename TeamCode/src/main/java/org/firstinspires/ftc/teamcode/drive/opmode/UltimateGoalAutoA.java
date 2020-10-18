0ce86f68a4e9c9162c2e53d3a25151af57b9e5d9
jeversmann <josh.eversmann@gmail.com>
2020-10-17 21:47:19 -0500

    Copy over initial test autos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/UltimateGoalAutoA.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.drive.SampleTankDrive;
+
+/*
+ * This is an example of a more complex path to really test the tuning.
+ */
+@Autonomous(group = "drive")
+public class UltimateGoalAutoA extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        // This is the pose of the robot centered on the left starting line against the wall
+        Pose2d startPose = new Pose2d(-63, -46, 0);
+        drive.setPoseEstimate(startPose);
+
+        Trajectory traj = drive.trajectoryBuilder(startPose)
+                .splineTo(new Vector2d(12-4, -58), 0)
+                .build();
+
+        drive.followTrajectory(traj);
+    }
+}
